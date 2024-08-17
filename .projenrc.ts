9babba2279542dd89e155cd54d12fdc76f848b67
Ryotaro <ryotaro612.ooo@gmail.com>
2024-08-17 16:33:42 +0900

    chore: project created with projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,7 +1,10 @@
 import { Project } from 'projen';
-
 const project = new Project({
-  name: 'my-projen-project',
-});
+  name: 'temp',
 
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
 project.synth();
