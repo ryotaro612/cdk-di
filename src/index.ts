769c222142068c697e2294ea64d7d61a9a37314c
Ryotaro <ryotaro612.ooo@gmail.com>
2024-08-18 19:35:12 +0900

    enter

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,6 @@
-import { BitBucketSourceCredentials } from 'aws-cdk-lib/aws-codebuild';
 import { IRepository, Repository } from 'aws-cdk-lib/aws-ecr';
 import { Bucket, IBucket } from 'aws-cdk-lib/aws-s3';
 import { IConstruct } from 'constructs';
-
-
-export function inject(deps: DepMap) {
-	deps
-	return;
-}
-
-interface DepMap {
-	[id: string]: Dep<IConstruct>
-}
-
-
-type Dep<T extends IConstruct> = IConstruct | InterDep<T>
-
-
-type Injectable<T extends IConstruct> = (mp: IConstructMap<T>) => T
-
-interface InterDep<T extends IConstruct> {
-	deps: string[],
-	injectable: Injectable<T>
-}
-
-
-interface IConstructMap<T extends IConstruct> {
-	[id: string]: T
-}
-
-interface A extends IConstructMap<IConstruct> {
-	a: IRepository
-	c: IBucket
-}
-
-
-function a(mp: A): Repository {
-	let c: any
-	mp.a
-	return c
-}
-a
-
-
-inject({
-});
-
-function tnject<T extends {}, A extends IConstruct, R extends { [id: string]: A }>(deps: { [id: string]: Bar<T> }): R {
-	deps
-	return 1 as any
-}
-
-type Ava<D extends {}, T extends IConstruct> = { deps?: (keyof D)[], injectable: (mp: D) => T }
-
-// function oject<D extends {}, T extends IConstruct, P extends {[id: string]: Ava<D, T>}>(p: P): {[ id in keyof P]: (Ava<D, T> extends {injectable: (mp: D)=> T} ? ReturnType<P[id]["injectable"]> : T)} {
-//   const res: {[a in keyof P]: IConstruct} = {} as any;
-//   res['a' as keyof P] = new Repository(1 as any, 'id')
-//   for(const [k, v] of Object.entries(p)) {
-// 	k
-// 	v
-//   }
-//   return res as any;
-// }
-
-function aject<D extends {}, T extends IConstruct, P extends { [id: string]: Ava<D, T> }>(p: P): { [id in keyof P]: ReturnType<P[id]["injectable"]> } {
-	const res: { [a in keyof P]: IConstruct } = {} as any;
-	res['a' as keyof P] = new Repository(1 as any, 'id')
-	for (const [k, v] of Object.entries(p)) {
-		k
-		v
-	}
-	return res as any;
-}
-
-
-
-
 
 
 type Foo<T extends { [id: string]: IConstruct }> = {
@@ -101,13 +26,13 @@ const d: Foo<{ a: IRepository }> = {
 const e: Foo<{ a: IRepository, d: IBucket }> = {
   deps: ['a', 'd'],
 	injectable: (mp) => {
-		return new Bucket(mp.a, 'id')
+		return new Bucket(mp['a'], 'id')
 	}
 }
 
 
 
-function eject<T extends { [id: string]: IConstruct }, P extends { [id in string]: Foo<T> }>(p: { [id in string]: Foo<T>}) {
+function eject<T extends { [id in string]: IConstruct }, P extends { [id in string]: Foo<T> }>(p: { [id in string]: Foo<T>}) {
 	const res: { [a in keyof P]: IConstruct } = {} as any;
 	res['a' as keyof P] = new Repository(1 as any, 'id')
 	for (const [k, v] of Object.entries(p)) {
@@ -123,15 +48,4 @@ const res = eject({
   x: e,
 });
 res
-
-tnject({
-	"doge": c,
-	"c": (1 as any) as Repository
-});
-
-// function f(scope: Construct) {
-//   const a = new Repository(scope, 'id')
-//  const uid: string = Names.uniqueId(myConstruct);
-// }
-
 
